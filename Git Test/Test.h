41fafb7ec8546fcaab4e0fbdc40ab2d92cc4a3d7
dear martinez <dearmartinez@icloud.com>
2014-10-23 19:50:13 -0400

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by dear martinez on 10/23/14.
+//  Copyright (c) 2014 dear martinez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by dear martinez on 10/23/14.
+//  Copyright (c) 2014 dear martinez. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
